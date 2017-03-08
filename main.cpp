c5c2de188dc0111a9ff97c5d2013ebe0ec06fe99
Patrick S. Watters <pwatters@ups.com>
2017-03-07 23:04:12 -0500

    created table sizes

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 /* Main.cpp */
 
-#include "predictor.h"
+#include "predictors.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
 	if(argc != 3){ 
-		cerr << "Accepts three files: ./predictor input.txt output.txt\n";
+		cerr << "Accepts only three files: ./predictor input.txt output.txt\n";
 		exit(1);
 	}
 
-	Predictor predictor; //initialize object
+	Predictors predictor; //initialize object
 
-	predictor.read_file();
+	predictor.read_file(argv[1]);
 	predictor.always_taken();
-	predictor.not_always_taken():
+	predictor.not_always_taken();
 
+	int* tables = { new int[16],  new int[32],  new int[128], 
+					new int[256], new int[512],	new int[1024], new int[2048] }
+	
+	predictor.bimodial_single_bit(tables);
+	//predictor.bimodial_double_bit(tables);
 
+	predictor.write_file(argv[2]);
 
-	predictor.write_file();
-
-	cout << "Success - output.txt written\n";
+	cout << "Success - output.txt written.\n";
 	return 0;
 }
